c841c4cdb92cab961eed319316463bc8c018c832
Harry Garrood <hdgarrood@gmail.com>
2013-05-08 19:18:44 +0100

    initial commit

---
+++ lib/redmine_acts_as_taggable_on.rb
@@ -0,0 +1,5 @@
+require "redmine_acts_as_taggable_on/version"
+
+module RedmineActsAsTaggableOn
+  # Your code goes here...
+end
---
+++ lib/redmine_acts_as_taggable_on/version.rb
@@ -0,0 +1,3 @@
+module RedmineActsAsTaggableOn
+  VERSION = "0.0.1"
+end
