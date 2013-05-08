198e53f3c75357d70bf4af35db24d83dde4ac382
Harry Garrood <hdgarrood@gmail.com>
2013-05-08 21:08:35 +0100

    refactor initialize.rb

--- lib/redmine_acts_as_taggable_on/initialize.rb
+++ lib/redmine_acts_as_taggable_on/initialize.rb
@@ -3,6 +3,14 @@ require 'acts-as-taggable-on'
 require 'redmine_acts_as_taggable_on/migration'
 require 'redmine_acts_as_taggable_on/redmine_plugin_patch'
 
-unless Redmine::Plugin.included_modules.include? RedmineActsAsTaggableOn::RedminePluginPatch
-  Redmine::Plugin.send(:include, RedmineActsAsTaggableOn::RedminePluginPatch)
+module RedmineActsAsTaggableOn
+  def self.initialize
+    unless @initialized
+      Redmine::Plugin.send(:include,
+                           RedmineActsAsTaggableOn::RedminePluginPatch)
+    end
+    @initialized = true
+  end
 end
+
+RedmineActsAsTaggableOn.initialize
