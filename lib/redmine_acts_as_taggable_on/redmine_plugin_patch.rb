cf08335b67c65d3326b085c5bb173aba9207d3e3
Harry Garrood <hdgarrood@gmail.com>
2013-05-08 21:46:41 +0100

    add warnings for acts-as-taggable-on

--- lib/redmine_acts_as_taggable_on/redmine_plugin_patch.rb
+++ lib/redmine_acts_as_taggable_on/redmine_plugin_patch.rb
@@ -4,8 +4,33 @@ module RedmineActsAsTaggableOn::RedminePluginPatch
   end
 
   def requires_acts_as_taggable_on?
-    # TODO: grep through Gemfile for acts-as-taggable-on in order to work with
-    # plugins which don't use this gem
-    !!@requires_acts_as_taggable_on
+    return true if @requires_acts_as_taggable_on
+
+    # Safety net: If the plugin uses the acts-as-taggable-on gem the old way,
+    # assume that it requires the tables.
+    if File.exist?(gemfile_path)
+      if File.read(gemfile_path).include? 'acts-as-taggable-on'
+        warn_about_acts_as_taggable_on
+        return true
+      end
+    end
+
+    false
+  end
+
+  private
+  def gemfile_path
+    File.join(self.directory, 'Gemfile')
+  end
+
+  def warn_about_acts_as_taggable_on
+    unless @already_warned_about_acts_as_taggable_on
+      msg = "\nWARNING: The plugin #{self.id} is using 'acts-as-taggable-on',\n"
+      msg << "which means that it might accidentally delete some of your data\n"
+      msg << "when you uninstall it. You should badger its maintainer to switch\n"
+      msg << "to https://github.com/hdgarrood/redmine_acts_as_taggable_on.\n\n"
+      $stderr.puts msg
+    end
+    @already_warned_about_acts_as_taggable_on = true
   end
 end
