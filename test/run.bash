e0d5b0a808cb98ec938c3cefe01434966488bc0f
Harry Garrood <hdgarrood@gmail.com>
2013-05-11 18:39:47 +0100

    quieter tests

--- test/run.bash
+++ test/run.bash
@@ -2,7 +2,7 @@
 set -e
 
 export redmine_acts_as_taggable_on_path="$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. && pwd)"
-pushd "$redmine_acts_as_taggable_on_path"
+pushd "$redmine_acts_as_taggable_on_path" >/dev/null
 
 export temp_redmine_path="`test/mk_temp_redmine.bash`"
 [ -f test/bats/bin/bats ] || git submodule update
@@ -11,5 +11,5 @@ test_status=0
 test/bats/bin/bats test/test.bats || test_status=1
 rm -rf "$temp_redmine_path"
 
-popd
+popd >/dev/null
 exit $test_status
