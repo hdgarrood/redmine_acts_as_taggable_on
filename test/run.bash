18279b74c4239952cfee4722ef94b83fb380b415
Harry Garrood <hdgarrood@gmail.com>
2013-05-11 18:27:17 +0100

    improve run.bash

--- test/run.bash
+++ test/run.bash
@@ -5,7 +5,8 @@ export redmine_acts_as_taggable_on_path="$(cd "$(dirname "${BASH_SOURCE[0]}")"/.
 pushd "$redmine_acts_as_taggable_on_path"
 
 export temp_redmine_path="`test/mk_temp_redmine.bash`"
-
+[ -f test/bats/bin/bats ] || git submodule update
 test/bats/bin/bats test/test.bats
+rm -rf "$temp_redmine_path"
 
 popd
