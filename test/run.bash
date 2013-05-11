ff3ae19420c08a840a5544d4a986d2bf92049c60
Harry Garrood <hdgarrood@gmail.com>
2013-05-11 19:11:17 +0100

    hopefully fix travis

--- test/run.bash
+++ test/run.bash
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+export RAILS_ENV=development
+
 export redmine_acts_as_taggable_on_path="$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. && pwd)"
 pushd "$redmine_acts_as_taggable_on_path" >/dev/null
 
@@ -9,7 +11,13 @@ export temp_redmine_path="`test/mk_temp_redmine.bash`"
 
 test_status=0
 test/bats/bin/bats test/test.bats || test_status=1
-rm -rf "$temp_redmine_path"
 
+rm -rf "$temp_redmine_path"
 popd >/dev/null
+
+if [ "$test_status" -ne 0 ]; then
+    echo "test output was:"
+    cat /tmp/bats.*.out
+fi
+
 exit $test_status
