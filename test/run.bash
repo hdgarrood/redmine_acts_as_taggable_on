613457386853aa70b431f5349277a729f4277fb7
Harry Garrood <hdgarrood@gmail.com>
2013-05-11 22:18:48 +0100

    remove unnecessary code

--- test/run.bash
+++ test/run.bash
@@ -10,7 +10,7 @@ pushd "$redmine_acts_as_taggable_on_path" >/dev/null
 
 temp_redmine_path="`test/mk_temp_redmine.bash`"
 export temp_redmine_path
-pushd "$redmine_acts_as_taggable_on_path" >/dev/null
+
 [ -f test/bats/bin/bats ] || git submodule update
 
 test_status=0
@@ -18,10 +18,5 @@ test/bats/bin/bats test/test.bats || test_status=1
 
 rm -rf "$temp_redmine_path"
 popd >/dev/null
-
-if [ "$test_status" -ne 0 ]; then
-    echo "test output was:"
-    cat /tmp/bats.*.out
-fi
 
 exit $test_status
